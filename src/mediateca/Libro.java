36119beb98d2aaa6f59bf7a2e8b5bebbdfe31be4
Houssamelbadaoui <helbad@aulaestudio.es>
2026-03-12 17:16:21 +0100

    Add class Libro

---
+++ src/mediateca/Libro.java
@@ -0,0 +1,28 @@
+package mediateca;
+
+public class Libro extends ItemBiblioteca{
+
+    private String autor;
+
+    // constructor
+    public Libro(String id, String titulo,boolean estadoPrestamo, String autor){
+        super(id, titulo, estadoPrestamo);
+        this.autor = autor;
+    }
+
+    @Override
+    public double calcularMulta(int diasRetraso) {
+        return diasRetraso * 0.50;
+    }
+
+    @Override
+    public int getDiasMaximoPrestamo() {
+        return 14;
+    }
+
+    @Override
+    public String toString(){
+        return  super.toString() +
+                " | Tipo: Libro | Autor: " + autor;
+    }
+}
